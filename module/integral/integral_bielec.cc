b349186738b0c6fc4772e3e8c93f8eb0c689ed0c
TApplencourt <tapplencourt@gmail.com>
2016-06-20 17:01:09 +0200

    fix: integral_bielec working

--- module/integral/integral_bielec.cc
+++ module/integral/integral_bielec.cc
@@ -1,36 +1,45 @@
 #include <libint2.hpp>
+#include <iostream> 
+using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char * argv[]) {
+
 
 
     /*** =========================== ***/
     /*** initialize molecule         ***/
     /*** =========================== ***/
-    std::string ezfio_filename = "";
-    std::string xyz_path = ezfio_filename + std::string("/libint/xyz");
-    // read geometry from a filename
+    std::string xyz_path = argv[1];
     std::ifstream input_file(xyz_path);
-    std::vector<libint2::Atom> atoms = libint2::read_dotxyz(input_file);
+    std::vector < libint2::Atom > atoms = libint2::read_dotxyz(input_file);
 
     /*** =========================== ***/
     /*** create basis set            ***/
     /*** =========================== ***/
+    std::string basis_name = argv[2];
+    //export LIBINT_DATA_PATH="$qp_root"/usr/share/libint/2.1.0-beta/basis/
 
-    std::string basis_path = ezfio_filename + std::string("/libint");
-    setenv("LIBINT_DATA_PATH", basis_path.c_str(), 1);
+    libint2::BasisSet obs(basis_name, atoms);
+    obs.set_pure(false); // use cartesian gaussians
 
-    libint2::BasisSet shells("basis", atoms);
-    shells.set_pure(false); // use cartesian gaussians
+    libint2::init(); // safe to use libint now
 
+    libint2::TwoBodyEngine < libint2::Coulomb > coulomb_engine(obs.max_nprim(), obs.max_l(), 0);
 
-  libint2::init();  // safe to use libint now
+    for (auto s1 = 0; s1 != obs.size(); ++s1) {
+        std::cout << s1 << "/" << obs.size() << std::endl;
+        for (auto s2 = 0; s2 != obs.size(); ++s2) {
+            for (auto s3 = 0; s3 != obs.size(); ++s3) {
+                for (auto s4 = 0; s4 != obs.size(); ++s4) {
 
-  // all other code snippets go here
+                    const auto * buf = coulomb_engine.compute(obs[s1], obs[s2], obs[s3], obs[s4]);
 
-  libint2::finalize();  // do not use libint after this
+                }
+            }
+        }
+    }
 
-  // can repeat the libint2::initialize() ... finalize() cycle as many times as
-  // necessary
+    libint2::finalize(); // do not use libint after this
 
-  return 0;
+    return 0;
 }
